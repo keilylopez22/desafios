793e1f80fda99cd6469845358725e5746c2c5d69
keilylopez22 <keilysamanta125@gmail.com>
2024-03-01 20:37:57 -0600

    subiendo desafios

---
+++ entero/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        try
+        {
+            while (true)
+            {
+                Console.Write("ingrese el primer numero: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("ingrese el segundo numero: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+
+                int resultado = (num1 + num2);
+                Console.WriteLine(resultado);
+            }
+        }
+        catch (FormatException ex)
+        {
+            Console.WriteLine($"error : ingresa un numero valido.");
+        }
+        finally
+        {
+            Console.WriteLine("cierre el programa");
+        }
+    }
+}
---
+++ promed/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("hola bienvenido al programa promedio Nos ingresas tu nombre por favor: ");
+        String nombre;
+        nombre = Console.ReadLine();
+        Console.WriteLine("el nombre introducido es: " + nombre);
+
+        int salario1, salario2, salario3;
+        Console.Write(nombre + "ingresa el salario del primer mes: ");
+        salario1 = int.Parse(Console.ReadLine());
+        Console.Write(nombre + "ingresa el salario del segundo mes: ");
+        salario2 = int.Parse(Console.ReadLine());
+        Console.Write(nombre + "ingresa el salario del tercer mes: ");
+        salario3 = int.Parse(Console.ReadLine());
+
+        Console.Write("el resultado de la suma de los tres salarios ingresados es: " + (salario1 + salario2 + salario3));
+
+        double Promedio = (salario1 + salario2 + salario3) / 3.0;
+        Console.WriteLine("el promedio total de tus salarios es de: " + Promedio);
+
+    }
+
+
+}
